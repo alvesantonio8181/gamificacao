d5fdcd4d49fee9071aa01185f654b7554505080b
Antonio <antonioaugusto.alves8181@gmail.com>
2026-01-17 21:43:57 -0300

    Exemplo de classe simples em Python

---
+++ curso.py
@@ -0,0 +1,6 @@
+class Curso:
+  def __init__(self, nome, descricao, preco, carga_horaria):
+    self.nome = nome
+    self.descricao = descricao
+    self.preco = preco
+    self.carga_horaria = carga_horaria
